3c3b6f9697bbb6cb81a3eb5cf9e2885a8a333a5b
Mikhail Vazhnov <michael.vazhnov@gmail.com>
2025-10-09 00:10:43 +0300

    update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tsEslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tsEslint.config(
   eslint.configs.recommended,
   tsEslint.configs.recommended,
 
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
-      semi: ['warn', 'never'],
+      'semi': 'off', // Disable the standard ESLint semi rule
+      '@stylistic/semi': ['error', 'never'], // Use the TypeScript-specific semi rule
+      'no-unreachable': 'error',
+      'no-unexpected-multiline': 'error',
       '@typescript-eslint/no-unused-vars': 'off',
     },
   },
-);
+)
